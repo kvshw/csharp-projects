f1ec56e90929f11e48ba517681a54d6710de7420
kvshw <kaveebhashiofficial@gmail.com>
2024-02-02 09:10:18 +0200

    code added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace YouTube
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            int numberOfNums = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= numberOfNums; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+
+}
